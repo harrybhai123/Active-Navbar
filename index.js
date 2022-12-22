b44ab5aefcd759c6ba51e4d01ac7e0ee6f0ab80d
Hariom Tomar <68967590+harrybhai123@users.noreply.github.com>
2022-12-22 13:27:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const pages = document.getElementById("pages");
+const items = document.querySelectorAll(".list-item");
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", function () {
+    let active = document.querySelectorAll(".active");
+    active[0].className = active[0].className.replace("active", "");
+    this.className += " active";
+    console.log(this.className);
+  });
+}
